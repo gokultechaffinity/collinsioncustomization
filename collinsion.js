62409df291ee6c9022b385e79d841a8f0b5f9c99
therealjedeye98 <sikhokhele.khohliso@collinsongroup.com>
2022-11-03 17:26:49 +0200

    Update collinsion.js

--- collinsion.js
+++ collinsion.js
@@ -129,7 +129,7 @@ jQuery(document).ready(function ($) {
     var DOBParent = document.getElementById(
       "helpdesk_ticket_custom_field_cf_date_of_birth_2321673"
     ).parentElement;
-    PostcodeParent.classList.add("for-section-1");
+    DOBParent.classList.add("for-section-1");
   }
 
   //add save and continue button - add section 1
@@ -142,6 +142,11 @@ jQuery(document).ready(function ($) {
       '<button id="save_and_continue1" class="btn btn-primary for-section-1 save_and_continue" type="button">Find Policy</button>'
     ).insertAfter("#helpdesk_ticket_custom_field_cf_postcode68273_2321673");
   } else if ($("#title").hasClass("CollinsonUK")) {
+    $(
+      '<button id="save_and_continue1" class="btn btn-primary for-section-1 save_and_continue" type="button">Find Policy</button>'
+    ).insertAfter("#helpdesk_ticket_custom_field_cf_date_of_birth_2321673");
+  }
+   else if ($("#title").hasClass("VHI")) {
     $(
       '<button id="save_and_continue1" class="btn btn-primary for-section-1 save_and_continue" type="button">Find Policy</button>'
     ).insertAfter("#helpdesk_ticket_custom_field_cf_date_of_birth_2321673");
