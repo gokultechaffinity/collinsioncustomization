ec16d581808348a360b2fa6b759d5ee0cf40abcd
gokultechaffinity <113661425+gokultechaffinity@users.noreply.github.com>
2022-11-11 12:12:09 +0530

    Added Your Documentation on ticket details page

--- collinsion-test.js
+++ collinsion-test.js
@@ -273,26 +273,45 @@ jQuery(document).ready(function ($) {
     //for-section-5 attachment group
     //___________________________________new_section 5_______________________________________________________
   
-    $(
-      '<div id="mock-doc" class="form-group attachments-container for-section-5"><span class="d-none"><input id="upload_file" type="file" multiple="false"><input id="files_list" type="file" name="helpdesk_ticket[attachments][][resource]" multiple="false"></span><div class="dropdown"><button class="btn btn-link " type="button" id="attach_btn" data-attach-type="file"><span class="file-upload-btn" tabindex="0"><img data-src="/glyphs/vectors/attachment.svg" loading="lazy" alt="Attachment" class="icon ls-is-cached lazyloaded" src="/glyphs/vectors/attachment.svg">Attachment</span></button><ul class="dropdown-menu" aria-labelledby="attach_btn"><li><a class="dropdown-item" href="#" data-attach-type="file">Attach a file</a></li></ul></div><div id="attachments_list" class="files-container"></div><div><div id="cumulative_error_heading" class="error-heading"></div><div id="cumulative_error_attachments_list" class="files-container"></div></div><div><div id="individual_error_heading" class="error-heading"></div><div id="individual_error_attachments_list" class="files-container"></div></div></div>'
-    ).insertAfter(
-      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
-    );
-    $(".form-group.attachments-container").addClass("for-section-5");
   
-    if ($("#title").hasClass("ColumbusItaly")) {
-      $(
-        '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Salva e continua</button>'
-      ).insertAfter(".form-group.attachments-container.for-section-5");
-    } else {
-      $(
-        '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Save & Continue</button>'
-      ).insertAfter(".form-group.attachments-container.for-section-5");
-     jQuery(".attachments-container.for-section-5").after('<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Save Draft </button>');
-    }
-    $("#mock-doc").remove();
-    $(".save_and_continue5").first().hide();
-    $(".save_draft_5").first().hide();
+    if(jQuery(".fw-comments-wrapper .attachments").length>0){
+        $('<div id="mock-doc" class="form-group attachments for-section-5"><span class="d-none"><input id="upload_file" type="file" multiple="false"><input id="files_list" type="file" name="helpdesk_ticket[attachments][][resource]" multiple="false"></span><div class="dropdown"><button class="btn btn-link " type="button" id="attach_btn" data-attach-type="file"><span class="file-upload-btn" tabindex="0"><img data-src="/glyphs/vectors/attachment.svg" loading="lazy" alt="Attachment" class="icon ls-is-cached lazyloaded" src="/glyphs/vectors/attachment.svg">Attachment</span></button><ul class="dropdown-menu" aria-labelledby="attach_btn"><li><a class="dropdown-item" href="#" data-attach-type="file">Attach a file</a></li></ul></div><div id="attachments_list" class="files-container"></div><div><div id="cumulative_error_heading" class="error-heading"></div><div id="cumulative_error_attachments_list" class="files-container"></div></div><div><div id="individual_error_heading" class="error-heading"></div><div id="individual_error_attachments_list" class="files-container"></div></div></div>'
+      ).insertAfter(".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper");
+        $($(".attachments")[0].outerHTML).insertAfter(
+        ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+        );
+        $("#portal_ticket_form .attachments").addClass("for-section-5");
+        if ($("#title").hasClass("ColumbusItaly")) {
+          $(
+            '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Salva e continua</button>'
+          ).insertAfter(".attachments.for-section-5");
+        } else {
+          $(
+            '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Save & Continue</button>'
+          ).insertAfter(".attachments.for-section-5");
+         jQuery(".attachments.for-section-5").after('<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Save Draft </button>');
+        }
+      
+      }
+      else{
+          $('<div id="mock-doc" class="form-group attachments-container for-section-5"><span class="d-none"><input id="upload_file" type="file" multiple="false"><input id="files_list" type="file" name="helpdesk_ticket[attachments][][resource]" multiple="false"></span><div class="dropdown"><button class="btn btn-link " type="button" id="attach_btn" data-attach-type="file"><span class="file-upload-btn" tabindex="0"><img data-src="/glyphs/vectors/attachment.svg" loading="lazy" alt="Attachment" class="icon ls-is-cached lazyloaded" src="/glyphs/vectors/attachment.svg">Attachment</span></button><ul class="dropdown-menu" aria-labelledby="attach_btn"><li><a class="dropdown-item" href="#" data-attach-type="file">Attach a file</a></li></ul></div><div id="attachments_list" class="files-container"></div><div><div id="cumulative_error_heading" class="error-heading"></div><div id="cumulative_error_attachments_list" class="files-container"></div></div><div><div id="individual_error_heading" class="error-heading"></div><div id="individual_error_attachments_list" class="files-container"></div></div></div>'
+      ).insertAfter(".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper");
+         $(".form-group.attachments-container").addClass("for-section-5");
+        if ($("#title").hasClass("ColumbusItaly")) {
+          $(
+            '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Salva e continua</button>'
+          ).insertAfter(".form-group.attachments-container.for-section-5");
+        } else {
+          $(
+            '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Save & Continue</button>'
+          ).insertAfter(".form-group.attachments-container.for-section-5");
+         jQuery(".attachments-container.for-section-5").after('<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Save Draft </button>');
+        }
+      }
+      $("#mock-doc").remove();
+      $(".save_and_continue5").first().hide();
+      $(".save_draft_5").first().hide();
+    
   
     // addImages();
     //____________________________________________________SECTION 6 Start - payment details____________________________________________
