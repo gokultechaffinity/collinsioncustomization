c0ec0a6c1929f0f701ff6c11484e0b844312e1a7
Sikhokhele-Khohliso <sikhokhele.khohliso@collinsongroup.com>
2022-12-06 16:57:42 +0200

    Update collinsion.js

--- collinsion.js
+++ collinsion.js
@@ -704,7 +704,7 @@ jQuery(document).ready(function ($) {
   
  
 
-  console.log($('iframe[src="/support/recaptcha#6LdvejUUAAAAAEn6wjOFcPVRyQr4KtAJ03ltA1SQ"]').contents.find('recaptcha-anchor'));
+  console.log($('iframe[src="/support/recaptcha#6LdvejUUAAAAAEn6wjOFcPVRyQr4KtAJ03ltA1SQ"]').contents().find('recaptcha-anchor'));
 
 
 }
