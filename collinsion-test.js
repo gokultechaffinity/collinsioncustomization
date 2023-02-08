4814d10937e2d9a55a1b079898ddf764a9754518
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-02-08 15:37:08 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -414,11 +414,11 @@ jQuery(document).ready(function ($) {
     ).insertAfter(
       ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
     );
-    $("<hr class='for-section-4'>").insertAfter("#save_draft_4")
-    // $("#collapseSection4"
-    // ).append(
-    //   '<p class="for-section-4" style="text-align: center;" >If you "Save Claim & Exit", we will send you an activation email within 5 minutes to your portal where you can continue with your claim.</p>'
-    // );
+    $("<hr class='for-section-4'>").insertAfter("#save_draft_4");
+    $('<p class="for-section-4" style="text-align: center;" >If you "Save Claim & Exit", we will send you an activation email within 5 minutes to your portal where you can continue with your claim.</p>'
+    ).insertBefore(
+      hr.for-section-4
+    );
   }
   //____________________________________________________SECTION 5 Start - Your Documents____________________________________________
   //for-section-5 attachment group
