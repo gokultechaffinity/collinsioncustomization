2f89a912ece81722361fb496c7e14d3a9d112059
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-02-09 15:11:12 +0200

    save-draft-5 re-add

--- collinsion.js
+++ collinsion.js
@@ -480,7 +480,7 @@ jQuery(document).ready(function ($) {
       $(
         '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Next Section</button>'
       ).insertAfter(".form-group.attachments-container.for-section-5");
-      // $("<hr id ='hr5' class='for-section-5'>").insertAfter(".save_and_continue5.btn.btn-primary.for-section-5.save_and_continue:nth-of-type(2)");
+      // $("<hr id ='hr5' class='for-section-5'>").insertAfter("(".save_and_continue5.btn.btn-primary.for-section-5.save_and_continue:visible")");
       // $('<p class="for-section-5" style="margin-top: 20px;">If you "Save Claim &amp; Exit", we will send you an activation email within 5 minutes to your portal where you can continue with your claim.</p>'
       // ).insertBefore(
       //   '#hr5'
