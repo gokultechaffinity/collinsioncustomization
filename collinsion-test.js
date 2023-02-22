a5435141f920600c9c983dd9dcad9723a49cb3d9
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-02-22 14:53:37 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -5901,12 +5901,12 @@ jQuery(document).ready(function ($) {
     black_indexes = []
 
     for (let i = 0; i < list_of_fields.length; i++ ){
-      if ($("#"+list_of_fields[i]).val() != '' ){
-        red_indexes.pop(i);
-        black_indexes.push(i);
-      }else{
+      if ($("#"+list_of_fields[i]).val() == '' ){
         red_indexes.push(i);
         black_indexes.pop(i);
+      }else{
+        red_indexes.pop(i);
+        black_indexes.push(i);
       }
     }
     console.log("the following indexes are empty")
