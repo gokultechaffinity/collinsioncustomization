5f84d08e85186e4357d4868cb015c4a29480803e
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-03-15 10:33:16 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -590,6 +590,7 @@ jQuery(document).ready(function ($) {
   }
   //____________________________________________________SECTION 7 Start - Summary Section____________________________________________
   if ($(".edit_helpdesk_ticket").length > 0) {
+
     $("span.btn.fw-primary-button.new-ticket-dummy").addClass("for-section-7");
     
     //new 
@@ -599,26 +600,9 @@ jQuery(document).ready(function ($) {
         "<p id='summary_intro'>Ti preghiamo di dedicare un momento a ricontrollare le tue informazioni: l'accuratezza ci aiuta a elaborare rapidamente la tua richiesta</p>"
       ).insertBefore("#helpdesk_ticket_submit");
     } else {
-      $("#helpdesk_ticket_form_id").addClass("for-section-7");
-      $(".form-group.helpdesk_ticket_email").addClass("for-section-7");
-      //won't be found
-      $(".btn.fw-primary-button.new-ticket-submit-button").addClass(
-        "for-section-7"
-      );
-      //execute this
-      $("span.btn.fw-primary-button.new-ticket-dummy").addClass("for-section-7");
-      // $(
-      //   '<p id="summary_intro">Please take a moment to double check your information - accuracy helps us process your claim quickly.</p>'
-      // ).insertBefore("#collapseSection7");
-      // console.log("added before fake submit")
+      
       
     }
-    // add_section_1();
-    // add_section_2();
-    // add_section_3();
-    // add_section_4();
-    // add_section_5();
-    // add_section_6();
 
   } else {
     $("#helpdesk_ticket_form_id").addClass("for-section-7");
@@ -855,10 +839,18 @@ jQuery(document).ready(function ($) {
           "<p id='summary_intro'>Ti preghiamo di dedicare un momento a ricontrollare le tue informazioni: l'accuratezza ci aiuta a elaborare rapidamente la tua richiesta</p>"
         ).insertBefore(".form-group.helpdesk_ticket_email.for-section-7");
       } else {
-        $(
-          '<p id="summary_intro">Please take a moment to double check your information - accuracy helps us process your claim quickly.</p>'
-        ).insertBefore(".form-group.helpdesk_ticket_email.for-section-7");
-        
+
+        if ($(".edit_helpdesk_ticket").length > 0) {
+          $(
+            '<p id="summary_intro">Please take a moment to double check your information - accuracy helps us process your claim quickly.</p>'
+          ).insertBefore(".btn.fw-primary-button.ticket-detail-dummy.for-section-7");
+          
+        }else{
+          $(
+            '<p id="summary_intro">Please take a moment to double check your information - accuracy helps us process your claim quickly.</p>'
+          ).insertBefore(".form-group.helpdesk_ticket_email.for-section-7");
+          
+        } 
       }
     }
   }
