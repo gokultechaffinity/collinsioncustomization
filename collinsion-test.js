25c6569efb1bd41f68f0f5a1a33bfaeea4fae3a2
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-03-15 13:07:06 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -846,8 +846,8 @@ jQuery(document).ready(function ($) {
   $("#section-7-button").attr("data-toggle", "collapse");
   //open section 1 by default
   if ($(".edit_helpdesk_ticket").length > 0) {
+
     $("#section-4-button").click();
-    $("#section-7-button").click();
     $("#section-7-button").click();
   }else{
     $("#section-1-button").click();
