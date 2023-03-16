187275968e3c53b9308bfdf2c5192f1b42982b52
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-03-16 10:39:19 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -6968,29 +6968,46 @@ jQuery(document).ready(function ($) {
     //   }
      }
   });
+  // $(".save_and_continue5").click(function () {
+  //   //check if we are in continue page
+  //   if (jQuery(".fw-comments-wrapper").length > 0) {
+  //     //will execute in ticket detail page senario
+  //     if (
+  //       $("#new_helpdesk_note#attachments_list.proper-attachments-list").length > 0)  {
+  //       fileUploadCallback();
+  //     } else if ($(".fw-comments-wrapper.fw-attachment-item").length) {
+  //       //Need to add another condition for already had div if div has length then we need to open next section
+  //       open_next(5);
+  //       $("#section-5-button").children(":first").removeClass("fa-minus");
+  //       $("#section-5-button").children(":first").removeClass("fa-plus");
+  //       $("#section-5-button").children(":first").addClass("fa-pen");
+  //       $("#section-5-button").css("background-color", "#524954");
+
+  //     }
+  //   } else {
+  //     //will excute in create new ticket senario
+  //     fileUploadCallback();
+  //   }
+  // });
+
   $(".save_and_continue5").click(function () {
     if (jQuery(".fw-comments-wrapper").length > 0) {
       //will execute in ticket detail page senario
       if (
-        $("#new_helpdesk_note#attachments_list.proper-attachments-list")
+        $("#new_helpdesk_note #attachments_list .proper-attachments-list")
           .length
       ) {
         fileUploadCallback();
-      } else if ($(".fw-comments-wrapper.fw-attachment-item").length) {
+      } else if ($(".fw-comments-wrapper .fw-attachment-item").length) {
         //Need to add another condition for already had div if div has length then we need to open next section
         open_next(5);
-        $("#section-5-button").children(":first").removeClass("fa-minus");
-        $("#section-5-button").children(":first").removeClass("fa-plus");
-        $("#section-5-button").children(":first").addClass("fa-pen");
-        $("#section-5-button").css("background-color", "#524954");
-
       }
     } else {
       //will excute in create new ticket senario
       fileUploadCallback();
     }
   });
-
+  
   function fileUploadCallback() {
     var elem = document.getElementById("files_list");
     if (elem.files.length) {
