97d4b3df27961040969d6d108b279a41ad2a662a
Sikhokhele-Khohliso <104072627+Sikhokhele-Khohliso@users.noreply.github.com>
2023-03-17 10:20:50 +0200

    Update collinsion.js

--- collinsion.js
+++ collinsion.js
@@ -13178,9 +13178,9 @@ jQuery(document).ready(function ($) {
     console.log("Moving around");
 
     //essential - move to relevant sections
-  $(".card-body.ins-card:eq(0)").attr("id","collapseSection7");
-  $("#collapseSection7").appendTo(".bg-grey.fw-sidebar");
-  $(".card-body.ins-card:eq(1)").insertAfter("#summary"); 
+  // $(".card-body.ins-card:eq(0)").attr("id","collapseSection7");
+  // $("#collapseSection7").appendTo(".bg-grey.fw-sidebar");
+  // $(".card-body.ins-card:eq(1)").insertAfter("#summary"); 
     
   }
    
