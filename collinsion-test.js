5e8ecd4a03240ffa1810d9d3367369e55ea1cac4
Sikhokhele-Khohliso <Sikhokhele.Khohliso@collinsongroup.com>
2023-07-10 10:11:30 +0200

    move pause button

--- collinsion-test.js
+++ collinsion-test.js
@@ -424,12 +424,12 @@ jQuery(document).ready(function ($) {
     $(
       '<button id="save_draft_4" class="btn btn-primary for-section-4 " type="button">Salva reclamo ed esci</button>'
     ).insertAfter(
-      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      "#overall_div_4"
     );
     $(
       '<button id="save_and_continue4" class="btn btn-primary for-section-4 save_and_continue" type="button">Sezione successiva</button>'
     ).insertAfter(
-      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      "#overall_div_4"
     );
     $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
     $("<p class='for-section-4' style='margin-top: 20px;'>Se clicchi 'Salva sinistro' ti invieremo un'e-mail entro 5 minuti per l'attivazione del tuo account, dove potrai continuare con il tuo sinistro</p>"
@@ -440,12 +440,12 @@ jQuery(document).ready(function ($) {
     $(
       '<button id="save_draft_4" class="btn btn-primary for-section-4 " type="button">Pause Claim</button>'
     ).insertAfter(
-      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      "#overall_div_4"
     );
     $(
       '<button id="save_and_continue4" class="btn btn-primary for-section-4 save_and_continue" type="button">Continue</button>'
     ).insertAfter(
-      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      "#overall_div_4"
     );
     $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
     $('<p class="for-section-4" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
