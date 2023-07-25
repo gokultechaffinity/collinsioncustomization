b02cd2ceff0cccf26a7c00c56f67048be61eb3f7
Sikhokhele-Khohliso <Sikhokhele.Khohliso@collinsongroup.com>
2023-07-25 13:28:01 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -433,7 +433,7 @@ jQuery(document).ready(function ($) {
     );
     $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
     $("<p class='for-section-4' style='margin-top: 20px;'>Se clicchi 'Salva sinistro' ti invieremo un'e-mail entro 5 minuti per l'attivazione del tuo account, dove potrai continuare con il tuo sinistro</p>"
-    ).insertAfter(
+    ).insertBefore(
       '#save_draft_4'
     );
   } else {
@@ -449,7 +449,7 @@ jQuery(document).ready(function ($) {
     );
     $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
     $('<p class="for-section-4" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
-    ).insertAfter(
+    ).insertBefore(
       '#save_draft_4'
     );
   }
