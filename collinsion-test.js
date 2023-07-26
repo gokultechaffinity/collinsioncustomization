3a866f7079200b515dbe94656e9139501467dc7d
Sikhokhele-Khohliso <Sikhokhele.Khohliso@collinsongroup.com>
2023-07-26 09:50:28 +0200

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -12518,17 +12518,17 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  $(".new-ticket-submit-button").click(function () {
-    var summary_div = document.getElementById("collapseSection7").innerHTML;
-    //add condition to check if information had already been appended, if so, clear description field and add new information
-    if(($(".fr-element.fr-view").length) != 0){
-      $(".fr-element.fr-view").empty();
-      $(".fr-element.fr-view").append(summary_div);
-    }else{
-      $(".fr-element.fr-view").append(summary_div);
-    }
+  // $(".new-ticket-submit-button").click(function () {
+  //   var summary_div = document.getElementById("collapseSection7").innerHTML;
+  //   //add condition to check if information had already been appended, if so, clear description field and add new information
+  //   if(($(".fr-element.fr-view").length) != 0){
+  //     $(".fr-element.fr-view").empty();
+  //     $(".fr-element.fr-view").append(summary_div);
+  //   }else{
+  //     $(".fr-element.fr-view").append(summary_div);
+  //   }
     
-  });
+  // });
 
   //____________________________________________________SECTION 6 End____________________________________________
 
