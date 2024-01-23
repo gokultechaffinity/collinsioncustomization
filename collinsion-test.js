32b6d04345341a2665abedd942746c7dbdfad052
gokultechaffinity <113661425+gokultechaffinity@users.noreply.github.com>
2024-01-23 18:21:49 +0530

    Update collinsion-test.js
    Adding line trigger code inside next logic

--- collinsion-test.js
+++ collinsion-test.js
@@ -6382,32 +6382,32 @@ jQuery(document).ready(function ($) {
 
     //close current, open next
    // closed by me 
-  //  open_next(4);
+    open_next(4);
 
     //remove edit from first three sections
 
-    //$("#section-1-button").removeAttr("data-toggle");
-    //$("#section-2-button").removeAttr("data-toggle");
-    //$("#section-3-button").removeAttr("data-toggle");
+    $("#section-1-button").removeAttr("data-toggle");
+    $("#section-2-button").removeAttr("data-toggle");
+    $("#section-3-button").removeAttr("data-toggle");
 
-    // $("#section-1-button").children(":first").removeClass("fa-plus");
-    // $("#section-1-button").children(":first").removeClass("fa-minus");
-    // $("#section-1-button").children(":first").addClass("fa-check");
+    $("#section-1-button").children(":first").removeClass("fa-plus");
+    $("#section-1-button").children(":first").removeClass("fa-minus");
+    $("#section-1-button").children(":first").addClass("fa-check");
 
-    // $("#section-2-button").children(":first").removeClass("fa-plus");
-    // $("#section-2-button").children(":first").removeClass("fa-minus");
-    // $("#section-2-button").children(":first").addClass("fa-check");
+    $("#section-2-button").children(":first").removeClass("fa-plus");
+    $("#section-2-button").children(":first").removeClass("fa-minus");
+    $("#section-2-button").children(":first").addClass("fa-check");
 
-    // $("#section-3-button").children(":first").removeClass("fa-plus");
-    // $("#section-3-button").children(":first").removeClass("fa-minus");
-    // $("#section-3-button").children(":first").addClass("fa-check");
+    $("#section-3-button").children(":first").removeClass("fa-plus");
+    $("#section-3-button").children(":first").removeClass("fa-minus");
+    $("#section-3-button").children(":first").addClass("fa-check");
 
-    // $("#section-1-button").css("background-color", "#4DC367");
-    // $("#section-2-button").css("background-color", "#4DC367");
-    // $("#section-3-button").css("background-color", "#4DC367");
+    $("#section-1-button").css("background-color", "#4DC367");
+    $("#section-2-button").css("background-color", "#4DC367");
+    $("#section-3-button").css("background-color", "#4DC367");
     
-    $(".new-ticket-submit-button").trigger("click");
-    // window.location.href = "/support/tickets";
+    $("#helpdesk_ticket_submit").trigger("click");
+     window.location.href = "/support/tickets";
   });
 
   function validateMandatoryFields(list_of_fields) {
