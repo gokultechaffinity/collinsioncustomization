093d44497552525224770893eb0753d23bc339e1
gokultechaffinity <113661425+gokultechaffinity@users.noreply.github.com>
2024-02-20 16:26:53 +0530

    Update collinsion-test.js

--- collinsion-test.js
+++ collinsion-test.js
@@ -431,27 +431,42 @@ jQuery(document).ready(function ($) {
         "<p class='for-section-4' style='margin-top: 20px;'>Se clicchi 'Salva sinistro' ti invieremo un'e-mail entro 5 minuti per l'attivazione del tuo account, dove potrai continuare con il tuo sinistro</p>"
       ).insertBefore("#save_draft_4");
     } else {
-      $(
-        '<button id="save_draft_4" class="btn btn-primary for-section-4 " type="button">Pause</button>'
-      ).insertAfter(
-        ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
-      );
-      $(
-        '<button id="save_and_continue4" class="btn btn-primary for-section-4 save_and_continue" type="button">Continue</button>'
-      ).insertAfter(
-        ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
-      );
-      $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
-      $(
-        '<p class="for-section-4" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
-      ).insertBefore("#save_draft_4");
+      // $(
+      //   '<button id="save_draft_4" class="btn btn-primary for-section-4 " type="button">Pause</button>'
+      // ).insertAfter(
+      //   ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      // );
+      // $(
+      //   '<button id="save_and_continue4" class="btn btn-primary for-section-4 save_and_continue" type="button">Continue</button>'
+      // ).insertAfter(
+      //   ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+      // );
+      // $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
+      // $(
+      //   '<p class="for-section-4" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
+      // ).insertBefore("#save_draft_4");
+console.log("---------entering into else")
+ $(
+      '<button id="save_draft_4" class="btn btn-primary for-section-4 " type="button">Pause Claim</button>'
+    ).insertBefore(
+      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+    );
+    $(
+      '<button id="save_and_continue4" class="btn btn-primary for-section-4 save_and_continue" type="button">Continue</button>'
+    ).insertAfter(
+      ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
+    );
+    $("<hr id ='hr4' class='for-section-4'>").insertAfter("#save_draft_4");
+    $(
+      '<p class="for-section-4" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
+    ).insertAfter("#save_draft_4");
     }
   }
   //____________________________________________________SECTION 5 Start - Your Documents____________________________________________
   //for-section-5 attachment group
   //___________________________________new_section 5_______________________________________________________
   if (jQuery(".fw-comments-wrapper").length > 0) {
-    $(
+       $(
       '<div id="mock-doc" class="form-group attachments for-section-5"><span class="d-none"><input id="upload_file" type="file" multiple="false"><input id="files_list" type="file" name="helpdesk_ticket[attachments][][resource]" multiple="false"></span><div class="dropdown"><button class="btn btn-link " type="button" id="attach_btn" data-attach-type="file"><span class="file-upload-btn" tabindex="0"><img data-src="/glyphs/vectors/attachment.svg" loading="lazy" alt="Attachment" class="icon ls-is-cached lazyloaded" src="/glyphs/vectors/attachment.svg">Attachment</span></button><ul class="dropdown-menu" aria-labelledby="attach_btn"><li><a class="dropdown-item" href="#" data-attach-type="file">Attach a file</a></li></ul></div><div id="attachments_list" class="files-container"></div><div><div id="cumulative_error_heading" class="error-heading"></div><div id="cumulative_error_attachments_list" class="files-container"></div></div><div><div id="individual_error_heading" class="error-heading"></div><div id="individual_error_attachments_list" class="files-container"></div></div></div>'
     ).insertAfter(
       ".helpdesk_ticket_custom_field_cf_reason_for_claim_2321673_section_wrapper"
@@ -479,7 +494,7 @@ jQuery(document).ready(function ($) {
       $("p.for-section-5:eq(0)").hide();
     } else {
       jQuery(".attachments.for-section-5").after(
-        '<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Pause</button>'
+        '<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Pause Claim</button>'
       );
       $(
         '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Continue</button>'
@@ -488,11 +503,6 @@ jQuery(document).ready(function ($) {
         '<p class="for-section-5" style="margin-top: 20px;">If you pause your claim, all of the information you have given will be saved for you.</p>'
       ).insertAfter(".save_draft_5.btn.btn-primary.for-section-5:visible");
       $("p.for-section-5:eq(0)").hide();
-
-      $("p.for-section-5:eq(1)").hide();
-      $(".save_draft_5").hide();
-      $("p.for-section-4").hide();
-      $("#save_draft_4").hide();
     }
   } else {
     $(
@@ -515,7 +525,7 @@ jQuery(document).ready(function ($) {
       $("p.for-section-5:eq(0)").hide();
     } else {
       jQuery(".attachments-container.for-section-5").before(
-        '<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Pause</button>'
+        '<button class="save_draft_5 btn btn-primary for-section-5 " type="button">Pause Claim</button>'
       );
       $(
         '<button class="save_and_continue5 btn btn-primary for-section-5 save_and_continue" type="button" >Continue</button>'
